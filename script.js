f48a6315042b4affff60fc454519d5e51fed30eb
Emma Smith <emma.smith@fetch.ai>
2021-03-20 12:08:15 +0000

    Added list of episode titles

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  // console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (episode of episodeList){
+    var para = makeNodeForEpisode(episode)
+    rootElem.appendChild(para);
+  }
+}
+
+function makeNodeForEpisode(episode){
+    var para = document.createElement("div");
+    var title = document.createElement("h2");
+    const episodeNumber = episode.number.toString().padStart(2, '0');
+    const episodeSeason = episode.season.toString().padStart(2, '0');
+    title.innerText = `${episode.name} - S${episodeSeason}E${episodeNumber}`;
+
+    para.appendChild(title);
+    return para;
 }
 
 window.onload = setup;
