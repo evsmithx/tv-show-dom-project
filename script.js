761f25f552d31df47abe712c08f1c103c189a812
Emma Smith <emma.smith@fetch.ai>
2021-03-20 12:41:06 +0000

    Level 100 complete

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  // console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   for (episode of episodeList){
     var para = makeNodeForEpisode(episode)
     rootElem.appendChild(para);
   }
+  var source = document.createElement("div");
+  rootElem.appendChild(source);
+  source.innerHTML = "<p>Info from <a href=\"http://TVMaze.com\">TVMaze.com<\a><\p>";
 }
 
 function makeNodeForEpisode(episode){
-    var para = document.createElement("div");
+    var para = document.createElement("article");
     var title = document.createElement("h2");
     const episodeNumber = episode.number.toString().padStart(2, '0');
     const episodeSeason = episode.season.toString().padStart(2, '0');
     title.innerText = `${episode.name} - S${episodeSeason}E${episodeNumber}`;
 
     para.appendChild(title);
+    var info = document.createElement("div");
+    para.appendChild(info);
+    var elem = document.createElement("img");
+    info.appendChild(elem);
+    elem.src = episode.image.medium;
+
+    info.insertAdjacentHTML('beforeend', episode.summary);
     return para;
 }
 
